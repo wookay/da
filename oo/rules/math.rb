265468138c8147193b342156a711b5be8bb1724a
luis <wookay.noh@gmail.com>
2008-12-11 09:54:46 +0900

    math

--- oo/rules/math.rb
+++ oo/rules/math.rb
@@ -1,9 +1,9 @@
+require "#{File.dirname __FILE__}/pol"
 class String
   def eval
     Fixnum.send :eval, self rescue self
   end
 end
-require "#{File.dirname __FILE__}/pol"
 pol = Pol.new
 pol.cook = 'eval'
 pol.monad = %w{abs ceil chr class even? floor inspect integer? next nil? nonzero? odd? ord prec_f prec_i pred round size succ to_f to_i to_s zero?}
