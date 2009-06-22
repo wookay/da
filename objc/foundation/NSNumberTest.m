01b709b74abc40da62a03a9e4b285c8e09ad2d76
wookay <wookay.noh@gmail.com>
2009-06-22 11:06:33 +0900

    10,000

--- objc/foundation/NSNumberTest.m
+++ objc/foundation/NSNumberTest.m
@@ -7,12 +7,19 @@
 
 - (void) unittest {
 
-  NSNumber* number = [NSNumber numberWithInt:100];
-  [assert_equal int:100 int:[number intValue]];
-  [assert_equal int:100 b:number];
+  NSNumber* number = [NSNumber numberWithInt:10000000];
+  [assert_equal int:10000000 int:[number intValue]];
+  [assert_equal int:10000000 b:number];
   [assert_equal bool:true bool:[number boolValue]];
-  [assert_equal a:@"100" b:[number stringValue]];
-  [assert_equal a:@"100" b:number];
+  [assert_equal a:@"10000000" b:[number stringValue]];
+  [assert_equal a:@"10000000" b:number];
+
+  NSNumberFormatter *numberFormatter = [[NSNumberFormatter alloc] init];
+  [numberFormatter setFormat: @"#,###"];
+  [numberFormatter setGroupingSeparator:@","];
+  [assert_equal a:@"," b:[numberFormatter groupingSeparator]];
+  [assert_equal a:@"10,000,000" b:[numberFormatter stringForObjectValue:number]];
+  [numberFormatter release];
 
   [assert_equal float:0.9f float:1.0f - 0.1f];
   [assert_equal float:1.2f b:[NSNumber numberWithFloat:1.2f]];
